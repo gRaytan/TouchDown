62abaac8eebad532805490a03a076a5f38896c89
Meitar Byle <maytarb@gmail.com>
2017-07-26 03:11:06 +0300

    cr fix

--- app/src/main/java/com/touchdown/muchface/MyApplication.java
+++ app/src/main/java/com/touchdown/muchface/MyApplication.java
@@ -3,14 +3,11 @@ package com.touchdown.muchface;
 import android.app.Application;
 import android.graphics.Bitmap;
 import android.util.Log;
-import com.cloudinary.Cloudinary;
-import com.microsoft.projectoxford.face.FaceServiceRestClient;
+
 import com.touchdown.muchface.domain.DetectionManager;
-import com.touchdown.muchface.domain.FaceDetector;
-import com.touchdown.muchface.domain.FaceDetectorImpl;
 import com.touchdown.muchface.domain.PersonDetails;
-import java.util.HashMap;
-import java.util.Map;
+import com.touchdown.muchface.util.ApiUtils;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -20,35 +17,16 @@ public class MyApplication extends Application {
   private static String LOG_TAG = MyApplication.class.getSimpleName();
 
   private DetectionManager mDetectionManager;
-  private FaceDetector mFaceDetector;
 
   @Override
   public void onCreate() {
     super.onCreate();
 
-    mFaceDetector = getFaceDetector();
     mDetectionManager = new MyDetectionManager();
-  }
-
-  private FaceDetectorImpl getFaceDetector() {
-    return new FaceDetectorImpl(createFaceServiceRestClient(), createCloudinary());
   }
 
   public DetectionManager getDetectionManager() {
     return mDetectionManager;
-  }
-
-  private FaceServiceRestClient createFaceServiceRestClient() {
-    return new FaceServiceRestClient("https://southeastasia.api.cognitive.microsoft.com/face/v1.0",
-        "9cf871789f8e4ebcab54a1b6cbe2dd1d");
-  }
-
-  private Cloudinary createCloudinary() {
-    Map config = new HashMap();
-    config.put("cloud_name", "dzdz6bcb6");
-    config.put("api_key", "421814359539652");
-    config.put("api_secret", "tksYjyszMzH0nkV5yKwy3-l40mc");
-    return new Cloudinary(config);
   }
 
   private class MyDetectionManager implements DetectionManager {
@@ -71,7 +49,7 @@ public class MyApplication extends Application {
     private void doSend(Bitmap bitmap) {
       try {
         long issueTime = System.currentTimeMillis();
-        PersonDetails details = getFaceDetector().getDetails(bitmap);
+        PersonDetails details = ApiUtils.createFaceDetector().getDetails(bitmap);
         if (mListener != null && issueTime > mLastFireTime) {
           mLastFireTime = System.currentTimeMillis();
           mListener.onSuccess(details, bitmap);
